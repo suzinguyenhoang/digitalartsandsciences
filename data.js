d74699654c62aaa96b06b113da0aea444ffd6b24
suzinguyenhoang <suzi.nguyenhoang@gmail.com>
2026-06-12 22:22:04 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,182 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dcmmmmmm",
+      "name": "dcmmmmmm",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.4172080111461671,
+        "pitch": 0.16131103553028048,
+        "fov": 1.4398647332081382
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5755877202857622,
+          "pitch": 0.9326971026615176,
+          "rotation": 6.283185307179586,
+          "target": "2-dcm"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4627119179393828,
+          "pitch": 0.37250889459604153,
+          "title": "Backpack (Ba lô)",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.7080803718458437,
+          "pitch": 0.47892152622143236,
+          "title": "Documents",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.480288699961825,
+          "pitch": 0.5876249783044027,
+          "title": "A Special Belt",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-thiet-ke-chua-co-ten-5",
+      "name": "Thiết kế chưa có tên-5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.58999062829409,
+          "pitch": 0.9380905901012717,
+          "rotation": 6.283185307179586,
+          "target": "2-dcm"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.10995028275836205,
+          "pitch": 0.3377685639918191,
+          "title": "Hammock (võng)",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.20910198383495704,
+          "pitch": 0.7177855272696423,
+          "title": "Rubber sandals",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.106606360388792,
+          "pitch": 0.19867467004181094,
+          "title": "Grandpa",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.3322745777238865,
+          "pitch": 0.3467029702079554,
+          "title": "Grandma",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-dcm",
+      "name": "dcm",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21841386748751113,
+          "pitch": 0.725745244103555,
+          "rotation": 0,
+          "target": "0-dcmmmmmm"
+        },
+        {
+          "yaw": -2.982170749634925,
+          "pitch": 1.0005624253045369,
+          "rotation": 5.497787143782138,
+          "target": "1-thiet-ke-chua-co-ten-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1912100535969294,
+          "pitch": 0.4761050972039502,
+          "title": "Hat",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.7646987472236733,
+          "pitch": 0.22467957323332755,
+          "title": "Food container (Bi-đông)",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.9095725662545657,
+          "pitch": 0.3090864689085695,
+          "title": "Knife&nbsp;",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
